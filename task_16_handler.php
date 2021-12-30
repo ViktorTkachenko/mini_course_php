2e017c81fdf5d4046dddc01e43786cc5fc7255c6
Viktor Tkachenko <whitesquirrel87@gmail.com>
2021-12-30 15:19:44 +0200

    Downloads files and outputs them

---
+++ task_16_handler.php
@@ -0,0 +1,27 @@
+<?php
+$imagePath = 'img/demo/gallery/';
+
+if (!empty($_FILES['image']['name'])) {
+    $pathParts = pathinfo($_FILES['image']['name']);
+    $newFileName = uniqid() . '.' . $pathParts['extension'];
+    $newPathFile = $imagePath . $newFileName;
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $newPathFile)) {
+        $host = '127.0.0.1';
+        $db   = 'marlindev';
+        $user = 'root';
+        $pass = 'root';
+        $charset = 'utf8';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $pdo = new PDO($dsn, $user, $pass, $opt);
+
+        $stmt = $pdo->prepare('INSERT INTO images SET image = :image');
+        $stmt->execute(['image' => $imagePath . $newFileName]);
+    }
+}
+header('Location: task_16.php');
